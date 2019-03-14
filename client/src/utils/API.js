25f08805e7c590b42290dcc215b24864a8bc2ee7
Kristen Johanson <43449427+klbjklbj@users.noreply.github.com>
2019-03-14 10:07:10 -0700

    initial upload

---
+++ client/src/utils/API.js
@@ -0,0 +1,27 @@
+// front end api calls to database and google books API
+
+import axios from "axios";
+
+export default {
+
+  // call to google books API
+  // searchBooks: function(searchedTitle) {
+  //   return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + searchTitle);
+  // },
+  // Gets all books
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Gets the book with the given id
+  getBook: function(id) {
+    return axios.get("/api/books/" + id);
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
+  }
+};
